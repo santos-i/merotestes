b56e9ab7aee63bb8a216a9df1dc725fdadb53105
Igor Santos <igor.santos@oceanpact.com>
2024-10-25 08:56:28 -0300

    fix mean wind dir

---
+++ script/__init__.py
@@ -0,0 +1,17 @@
+INPUT_PATH = './input/'
+OUTPUT_PATH = './output/'
+
+TRANSLATOR_COLS= [
+    'date',
+    'time',
+    'wdir_1',
+    'wspeed_1',
+    'wdir_2',
+    'wspeed_2',
+    'pressure_1',
+    'pressure_2',
+    'temperature_1',
+    'humidity_1',
+    'temperature_2',
+    'humidity_2',
+]
